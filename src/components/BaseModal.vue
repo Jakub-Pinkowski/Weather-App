183707978423e72429f9863d6fe1493a7aa26298
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-07-19 21:43:02 +0200

    Added modal funcionality

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,14 +1,28 @@
 <template>
     <div
+        v-show="modalActive"
         class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
     >
-        <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
+        <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
             <slot> </slot>
-            <button class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+            <button
+                class="text-white mt-8 bg-weather-primary py-2 px-6"
+                @click="$emit('close-modal')"
+            >
+                Close
+            </button>
         </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineEmits(['close-modal'])
+defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
 
 <style scoped></style>
