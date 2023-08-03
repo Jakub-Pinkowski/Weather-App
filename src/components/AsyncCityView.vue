7cdd7bb6dbd89d2c538fb9976c076938313f2008
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-08-03 20:18:46 +0200

    Weather API working peoperly

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -12,19 +12,8 @@ const API = '5515a667829df13bed05d2662aaf92fe'
 const getWeatherData = async () => {
     try {
         const weatherData = await axios.get(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lon}&appid=${API}`
+            `https://api.openweathermap.org/data/2.5/weather?lat=${route.query.lat}&lon=${route.query.lon}&appid=${API}&units=metric`
         )
-
-        // Calculate current date and time
-        const localOffset = new Date().getTimezoneOffset() * 60000
-        const utc = weatherData.data.current.dt * 1000 + localOffset
-        weatherData.data.currentTime = utc + 1000 * weatherData.data.timezone_offset
-
-        // Calculate horly weather offset
-        weatherData.data.hourly.forEach((hour: any) => {
-            const utc = hour.dt * 1000 + localOffset
-            hour.currentTime = utc + 1000 * weatherData.data.timezone_offset
-        })
 
         return weatherData
     } catch (error) {
