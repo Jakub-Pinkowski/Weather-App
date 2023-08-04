57469099d5e40ff2dcb60981781635a6aa170357
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-08-04 12:37:16 +0200

    Testing live deployment v2

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -4,7 +4,7 @@
             <RouterLink :to="{ name: 'home' }">
                 <div class="flex items-center gap-3">
                     <i class="fa-solid fa-sun text-2xl"></i>
-                    <p class="text-2xl">The Local Sasasasas</p>
+                    <p class="text-2xl">The Weather</p>
                 </div>
             </RouterLink>
 
